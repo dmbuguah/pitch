dbcabcb093ff3de96547d8ee796bfdb2e94299db
dmbuguah <danmbugua74@gmail.com>
2020-03-21 01:25:53 +0300

    Scaffold project

---
+++ app/src/main/java/com/cdan/pitch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cdan.pitch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
