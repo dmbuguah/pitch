15acbe957dadf55986428c7336d061345538a883
dmbuguah <danmbugua74@gmail.com>
2020-03-21 03:00:44 +0300

    Set ring volume conditionally

--- app/src/main/java/com/cdan/pitch/receiver/PhoneStateReceiver.kt
+++ app/src/main/java/com/cdan/pitch/receiver/PhoneStateReceiver.kt
@@ -7,9 +7,10 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import android.content.Context.TELEPHONY_SERVICE
+import android.media.AudioManager
 import androidx.core.content.ContextCompat.getSystemService
 import android.telephony.TelephonyManager
-
+import androidx.core.content.ContextCompat.getSystemService
 
 
 private const val TAG = "PhoneStateReceiver"
@@ -21,8 +22,19 @@ class PhoneStateReceiver : BroadcastReceiver() {
         when (tm.callState) {
 
             TelephonyManager.CALL_STATE_RINGING -> {
-                val phoneNr = intent.getStringExtra("incoming_number")
+                val phoneNr:String = intent.getStringExtra("incoming_number")
+                Log.e(TAG, "Call before ringing");
                 Toast.makeText(context, phoneNr, Toast.LENGTH_LONG).show()
+                if (phoneNr != null && phoneNr.equals("0700000000")) {
+                    Log.e(TAG, "Call after ringing");
+                    Toast.makeText(context, phoneNr, Toast.LENGTH_LONG).show()
+                    val mobilemode =
+                        context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+                    mobilemode.setStreamVolume(
+                        AudioManager.STREAM_RING, mobilemode.getStreamMaxVolume(AudioManager.STREAM_RING),
+                        AudioManager.FLAG_ALLOW_RINGER_MODES or AudioManager.FLAG_PLAY_SOUND);
+
+                }
             }
         }
 
