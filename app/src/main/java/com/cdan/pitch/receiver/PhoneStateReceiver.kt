105be08a1d61011d00dc43e8b07b1ea62b853712
dmbuguah <danmbugua74@gmail.com>
2020-03-22 14:43:58 +0300

    Check for do not disturb permission

--- app/src/main/java/com/cdan/pitch/receiver/PhoneStateReceiver.kt
+++ app/src/main/java/com/cdan/pitch/receiver/PhoneStateReceiver.kt
@@ -1,5 +1,6 @@
 package com.cdan.pitch.receiver
 
+import android.app.NotificationManager
 import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -8,25 +9,35 @@ import android.util.Log
 import android.widget.Toast
 import android.content.Context.TELEPHONY_SERVICE
 import android.media.AudioManager
-import androidx.core.content.ContextCompat.getSystemService
+import android.os.Build
 import android.telephony.TelephonyManager
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.*
 
 
 private const val TAG = "PhoneStateReceiver"
 
 class PhoneStateReceiver : BroadcastReceiver() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context, intent: Intent) {
         val tm = context.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
         when (tm.callState) {
 
             TelephonyManager.CALL_STATE_RINGING -> {
                 val phoneNr:String = intent.getStringExtra("incoming_number")
-                Log.e(TAG, "Call before ringing");
-                Toast.makeText(context, phoneNr, Toast.LENGTH_LONG).show()
-                if (phoneNr != null && phoneNr.equals("0700000000")) {
-                    Log.e(TAG, "Call after ringing");
+                val notificationManager = context.getSystemService(
+                    Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                if (!notificationManager.isNotificationPolicyAccessGranted) {
+
+                    val notint = Intent(
+                        android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                    notint.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(context, notint, null);
+                }
+                if (phoneNr.equals("0700000000")) {
                     Toast.makeText(context, phoneNr, Toast.LENGTH_LONG).show()
                     val mobilemode =
                         context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
